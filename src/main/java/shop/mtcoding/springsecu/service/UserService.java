612f6a11a0331fa0abde499216aaf960b99bd2e0
aj124578 <aj124578@naver.com>
2023-02-21 16:36:29 +0900

    회원가입시 비밀번호 인코딩 완료

--- src/main/java/shop/mtcoding/springsecu/service/UserService.java
+++ src/main/java/shop/mtcoding/springsecu/service/UserService.java
@@ -9,6 +9,7 @@ import shop.mtcoding.springsecu.dto.user.UserReq.LoginReqDto;
 import shop.mtcoding.springsecu.handler.ex.CustomException;
 import shop.mtcoding.springsecu.model.UserRepository;
 import shop.mtcoding.springsecu.model.user.User;
+import shop.mtcoding.springsecu.util.PasswordHash;
 
 @Service
 public class UserService {
@@ -19,6 +20,9 @@ public class UserService {
     @Transactional 
     public void 회원가입(JoinReqDto joinReqDto){
         User sameUser = userRepository.findByUsername(joinReqDto.getUsername());
+        String pw = PasswordHash.getPasswordHash(joinReqDto.getPassword());
+        joinReqDto.setPassword(pw);
+        
 
         if (sameUser != null) {
             throw new CustomException("동일한 username이 존재합니다");
